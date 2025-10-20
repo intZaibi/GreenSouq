f46f758d7f56430c403d7cf9d30089f6a7c5dce9
intZaibi <shahzaibalisomroo@gmail.com>
2025-10-20 18:11:55 +0500

    minor fixes

--- greensouq-clone/src/app/api/favorites/route.ts
+++ greensouq-clone/src/app/api/favorites/route.ts
@@ -1,4 +1,4 @@
-import { authOptions } from "../auth/[...nextauth]/route";
+import { authOptions } from "@/../lib/auth";
 import { getServerSession } from "next-auth";
 import { NextResponse } from "next/server";
 import { prisma } from "../../../../lib/prisma";
